7fe27e9f055f6b29079edb31be0f6d3316e8a45b
ggoodstudydaysup <2502198967@qq.com>
2015-06-04 15:41:03 +0800

    done

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scrollMany
+//
+//  Created by zhuangman on 15/6/3.
+//  Copyright (c) 2015年 zhuangman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,47 @@
+//
+//  ViewController.m
+//  scrollMany
+//
+//  Created by zhuangman on 15/6/3.
+//  Copyright (c) 2015年 zhuangman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+@implementation ViewController
+
+#pragma mark - TableViewDelegate & TableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellID = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld==%ld", tableView.tag, indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
