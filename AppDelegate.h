80b4664099311544d5d92d6e7a90ea5e199aad0a
ggoodstudydaysup <2502198967@qq.com>
2015-06-04 15:38:51 +0800

    111

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollMany
+//
+//  Created by zhuangman on 15/6/3.
+//  Copyright (c) 2015年 zhuangman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
